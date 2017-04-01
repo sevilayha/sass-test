447dd666a249fa55921c0ff793096624679ea42c
Chris Sevilleja <chris@scotch.io>
2017-03-31 17:57:11 -0700

    ready for scripts stuff

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
   plugins  = require('gulp-load-plugins')();
 
 gulp.task('styles', () => {
-  gulp.src('./sass/**/*.scss')
+  return gulp.src('./sass/**/*.scss')
     .pipe(plugins.sass({
       outputStyle: 'compressed',
       includePaths: ['sass', 'node_modules/bootstrap-sass/assets/stylesheets']
@@ -10,7 +10,20 @@ gulp.task('styles', () => {
     .on('error', plugins.sass.logError)
     .pipe(plugins.autoprefixer())
     .pipe(plugins.rename('styles.css'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('scripts', () => {
+  return gulp.src([
+    './node_modules/algoliasearch/dist/algoliasearch.min.js',
+    './node_modules/autocomplete/lib/autocomplete.js',
+    './js/**/*.js'
+  ])
+    .pipe(plugins.plumber())
+    .pipe(plugins.babel({ presets: ['es2015'] }))
+    .pipe(plugins.uglify())
+    .pipe(plugins.concat('spells.min.js'))
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('watch', () => {
