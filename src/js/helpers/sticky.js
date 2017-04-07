45f55ba3ce38b4577c4dd52711cfc5fd6a6fe7c0
Chris Sevilleja <chris@scotch.io>
2017-04-07 14:20:06 -0600

    sticky done

--- src/js/helpers/sticky.js
+++ src/js/helpers/sticky.js
@@ -1,5 +1,11 @@
+// stick all the things
 const stickyElements = document.querySelectorAll('.stick-this');
 stickyElements.forEach(stickTheThing);
+
+// watch for the footer coming into view. unstick everything
+document.addEventListener('scroll', debounce(() => {
+  if (isInView('#site__footer')) unstickAllAtBottom();
+}, 10));
 
 /**
  * Make an element sticky once it hits the top of the browser
@@ -9,8 +15,9 @@ function stickTheThing(element) {
   const offsetTop = element.dataset.offsetTop;
   wrapTheStickyThing(element);
   document.addEventListener('scroll', debounce(() => {
+
     watchScrollForStick(element, offsetTop);
-  }, 15));
+  }, 10));
 }
 
 /**
@@ -38,15 +45,43 @@ function wrapTheStickyThing(element) {
  * @param {number} offset How far off the top of the window we want it to stick
  */
 function watchScrollForStick(element, offsetTop = 0) {
-  console.count('doing');
+  if (isInView('#site__footer')) return;
+
   const topOfElement = element.getBoundingClientRect().top;
   const stuckThing   = element.querySelector('.stick-this');
 
   if ((topOfElement - offsetTop) <= 0) {
+    if (stuckThing.classList.contains('stuck')) return;
+
     stuckThing.style.top = `${offsetTop}px`;
     stuckThing.classList.add('stuck');
   } else {
+    if (!stuckThing.classList.contains('stuck')) return;
+
     stuckThing.style.top = 0;
     stuckThing.classList.remove('stuck');
   }
+}
+
+/**
+ * Once we hit the bottom of the page, destroy all sticky items
+ */
+function unstickAllAtBottom() {
+  const stickyElements = document.querySelectorAll('.stick-this.stuck');
+
+  stickyElements.forEach(element => {
+    // const topOfElement     = element.getBoundingClientRect().top;
+    // element.style.top      = `${topOfElement}px`;
+    // element.style.position = 'absolute';
+    element.classList.remove('stuck');
+  });
+}
+
+/**
+ * Check to see if the element is within the viewport
+ * @param {node} element The element to check
+ */
+function isInView(selector) {
+  const element = document.querySelector(selector);
+  return (element.getBoundingClientRect().top <= window.innerHeight);
 }
